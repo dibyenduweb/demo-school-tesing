e7db0640083642bca89a1258e970f68bd984aa29
mdshourovislam <mdsourovislam10001@gmail.com>
2026-04-16 12:45:25 +0600

    server

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ require('dotenv').config()
 
 const app = express()
 const Port = process.env.PORT || 3000
-app.listen(Port, )
+
+
+app.listen(Port, ()=>{
+    console.log(`Server is running on port ${Port}`)
+} )
